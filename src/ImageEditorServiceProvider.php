6de37376f3108fe03f894f75bbdd2b91177202c0
Ali Vaghey <a.vaghey0@gmail.com>
2025-09-30 12:34:44 +0330

    initialized the package

---
+++ src/ImageEditorServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Vaghey\ImageEditor;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
